5ed623c019525d39bf13fea1d75e8f934fab6040
Isaac Santana <isaacsantana.162@gmail.com>
2026-03-27 02:22:42 -0300

    Implemented all the working functionality, missing only polishment and styling.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,32 +25,45 @@ checkbox.addEventListener("change", () => {
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    
+
+    resultDiv.querySelector("h1").classList.remove("hidden");
+
     calculate(new FormData(form));
 
-    
+
 });
 
 function calculate(formData) {
     let counter = formData.get("itemsToCalculate");
-    let onSalesQuantity = formData.get("onSalesQuantity");
+    let onSaleQuantity = formData.get("onSaleQuantity");
     let desiredValue = formData.get("desiredValue");
 
-    let discount 
-    let valueOnSale
-    let finalDiscount
-    let finalValue
-    let valuePerUnit
-    
+    let discount;
+    let finalDiscount;
+    let finalValue;
+    let valuePerUnit;
+
     if (!discountInput.disabled) {
         discount = formData.get("discount")
     } else {
-        discount = 0;
+        let valueOnSale = formData.get("valueOnSale");
+        let originalValue = formData.get("originalValue");
+
+        let onSaleTotalValue = valueOnSale * onSaleQuantity;
+        let rawTotalValue = originalValue * onSaleQuantity;
+
+        discount = rawTotalValue - onSaleTotalValue;
     }
 
     for (let i = 1; i <= counter; i++) {
         let element = document.createElement("p")
-        element.innerText = ``
-        resultDiv.appendChild()
-    }   
+
+        finalDiscount = discount * Math.floor(i / onSaleQuantity);
+        valuePerUnit = ((i * desiredValue + finalDiscount) / i).toFixed(2);
+        finalValue = desiredValue * i;
+
+        console.log(`${i} --- ${valuePerUnit} --- ${finalValue}`)
+        element.innerText = `${i} --- R$ ${valuePerUnit} --- R$ ${finalValue}`;
+        resultDiv.appendChild(element)
+    }
 }
